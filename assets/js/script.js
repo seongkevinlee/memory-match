3b2dc979f322ed008ec60d5b43c35812c8e342ce
Seong Lee <seongkevinlee@gmail.com>
2020-04-22 18:09:14 -0700

    Added Media Queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,41 +27,41 @@ var cardFrontArray = [
   "docker-logo",
 ];
 var cardFronts = document.querySelectorAll(".card-front");
-var gameCards = document.querySelector('#gameCards');
-var startButton = document.querySelector('.startGame');
+var gameCards = document.querySelector("#gameCards");
+var startButton = document.querySelector(".startGame");
 
-startButton.addEventListener('click', createCards);
+startButton.addEventListener("click", createCards);
 
-gameCards.addEventListener('click', handleClick);
+gameCards.addEventListener("click", handleClick);
 function handleClick(event) {
-  if(event.target.className.indexOf('card-back') === -1) {
+  if (event.target.className.indexOf("card-back") === -1) {
     return;
   }
-  event.target.classList.add('hidden');
+  event.target.classList.add("hidden");
 
-  if(!firstCardClicked) {
+  if (!firstCardClicked) {
     firstCardClicked = event.target;
     firstCardClasses = firstCardClicked.previousElementSibling.className;
   } else {
     secondCardClicked = event.target;
     secondCardClasses = secondCardClicked.previousElementSibling.className;
-    gameCards.removeEventListener('click', handleClick);
+    gameCards.removeEventListener("click", handleClick);
     attempts++;
 
-    if(firstCardClasses === secondCardClasses) {
+    if (firstCardClasses === secondCardClasses) {
       gameCards.addEventListener("click", handleClick);
       firstCardClicked = null;
       secondCardClicked = null;
       matches++;
-      if(matches === maxMatches) {
-        var modal = document.querySelector(".modal")
+      if (matches === maxMatches) {
+        var modal = document.querySelector(".modal");
         modal.classList.remove("hidden");
       }
     } else {
-      setTimeout(function() {
-        firstCardClicked.classList.remove('hidden');
-        secondCardClicked.classList.remove('hidden');
-        gameCards.addEventListener('click', handleClick);
+      setTimeout(function () {
+        firstCardClicked.classList.remove("hidden");
+        secondCardClicked.classList.remove("hidden");
+        gameCards.addEventListener("click", handleClick);
         firstCardClicked = null;
         secondCardClicked = null;
       }, 1500);
@@ -71,24 +71,24 @@ function handleClick(event) {
 }
 
 function displayStats() {
-  var gamesPlayedNum = document.querySelector('#gamesPlayed');
+  var gamesPlayedNum = document.querySelector("#gamesPlayed");
   gamesPlayedNum.textContent = gamesPlayed;
-  var attemptsNum = document.querySelector('#attempts');
+  var attemptsNum = document.querySelector("#attempts");
   attemptsNum.textContent = attempts;
-  var accuracy = document.querySelector('#accuracy');
+  var accuracy = document.querySelector("#accuracy");
   accuracy.textContent = calculateAccuracy(attempts, matches);
 }
 
 function calculateAccuracy(attempts, matches) {
-  if(!attempts) {
+  if (!attempts) {
     return "0%";
   }
-  return Math.trunc(matches / attempts * 100) + "%";
+  return Math.trunc((matches / attempts) * 100) + "%";
 }
 
 //PLAY AGAIN BUTTON
 var playAgain = document.querySelector("#play-again");
-playAgain.addEventListener('click', resetGame);
+playAgain.addEventListener("click", resetGame);
 
 function resetGame() {
   matches = 0;
@@ -96,37 +96,39 @@ function resetGame() {
   gamesPlayed++;
   displayStats();
   resetCards();
-  document.querySelector(".modal").classList.add('hidden');
+  document.querySelector(".modal").classList.add("hidden");
 }
 
 function resetCards() {
   shuffleCards();
   displayCards();
-  var hiddenCards = document.querySelectorAll('.card-back');
-  for(var i = 0 ; i < hiddenCards.length ; i++) {
-    hiddenCards[i].classList.remove('hidden');
-    }
+  var hiddenCards = document.querySelectorAll(".card-back");
+  for (var i = 0; i < hiddenCards.length; i++) {
+    hiddenCards[i].classList.remove("hidden");
   }
+}
 
 function shuffleCards() {
-  for (let i = cardFrontArray.length - 1 ; i > 0; i--) {
+  for (let i = cardFrontArray.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
-    [cardFrontArray[i], cardFrontArray[j]] = [cardFrontArray[j], cardFrontArray[i]];
+    [cardFrontArray[i], cardFrontArray[j]] = [
+      cardFrontArray[j],
+      cardFrontArray[i],
+    ];
   }
 }
 
 function displayCards() {
-  for(var i = 0 ; i < cardFronts.length ; i++) {
-    cardFronts[i].className = 'card-front ' + cardFrontArray[i];
+  for (var i = 0; i < cardFronts.length; i++) {
+    cardFronts[i].className = "card-front " + cardFrontArray[i];
   }
 }
 
-
 function createCards() {
-  document.querySelector('.startModal').classList.add('hidden');
-  shuffleCards()
-  for(var i = 0 ; i < maxMatches * 2 ; i++) {
-    var cardContainer = document.createElement('div');
+  document.querySelector(".startModal").classList.add("hidden");
+  shuffleCards();
+  for (var i = 0; i < maxMatches * 2; i++) {
+    var cardContainer = document.createElement("div");
     cardContainer.className = "card col-2";
     var cardFront = createFrontCard(cardFrontArray[i]);
     var cardBack = createBackCard();
@@ -136,13 +138,13 @@ function createCards() {
 }
 
 function createFrontCard(className) {
-  var frontCard = document.createElement('div');
+  var frontCard = document.createElement("div");
   frontCard.classList.add("card-front", className);
   return frontCard;
 }
 
 function createBackCard() {
-  var backCard = document.createElement('div');
+  var backCard = document.createElement("div");
   backCard.classList.add("card-back");
   return backCard;
 }
