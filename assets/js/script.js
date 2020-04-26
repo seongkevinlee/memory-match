ad3853e058965ac36899c14e66ed1243ce8f6a6a
Seong Lee <seongkevinlee@gmail.com>
2020-04-25 22:15:39 -0700

    New Theme. Completed Appearance and card selectability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,140 @@
 var firstCardClicked;
 var secondCardClicked;
+var thirdCardClicked;
 var firstCardClasses;
 var secondCardClasses;
+var thirdCardClasses;
+var firstCardID;
+var secondCardID;
+var thirdCardID;
 var maxMatches = 9;
 var matches = 0;
 var attempts = 0;
 var gamesPlayed = 0;
+var costs = 1800;
+var revenue = 0;
+var netProfit = revenue - costs;
+var profitMargin = netProfit / costs;
+var dishID = 0;
+var bruschettaID = 6;
+var fettuccineID = 306;
+var saladID = 3006;
+var spaghettiID = 30006;
+var tartID = 300006;
+var tiramisuID = 3000006;
+
+
 var cardFrontArray = [
-  "react-logo",
-  "react-logo",
-  "php-logo",
-  "php-logo",
-  "node-logo",
-  "node-logo",
-  "mysql-logo",
-  "mysql-logo",
-  "html-logo",
-  "html-logo",
-  "github-logo",
-  "github-logo",
-  "js-logo",
-  "js-logo",
-  "css-logo",
-  "css-logo",
-  "docker-logo",
-  "docker-logo",
+  { class: "bruschetta-bread", text: "Bread", id: 1 },
+  { class: "bruschetta-okra", text: "Okra", id: 2 },
+  { class: "bruschetta-tomato", text: "Tomatoes", id: 3 },
+  { class: "fettuccine-noodles", text: "Fettuccine", id: 101 },
+  { class: "fettuccine-sauce", text: "Cream Sauce", id: 102 },
+  { class: "fettuccine-shrimp", text: "Shrimp", id: 103 },
+  { class: "salad-olives", text: "Olives", id: 1001 },
+  { class: "salad-pepperoni", text: "Pepperoni", id: 1002 },
+  { class: "salad-romaine", text: "Romaine", id: 1003 },
+  { class: "spaghetti-marinara", text: "Marinara Sauce", id: 10001 },
+  { class: "spaghetti-meatballs", text: "Meatballs", id: 10002 },
+  { class: "spaghetti-noodles", text: "Spaghetti", id: 10003 },
+  { class: "tart-crust", text: "Pie Crust", id: 100001 },
+  { class: "tart-peaches", text: "Peaches", id: 100002 },
+  { class: "tart-sugar", text: "Powdered Sugar", id: 100003 },
+  { class: "tiramisu-coffee", text: "Coffee", id: 1000001 },
+  { class: "tiramisu-ladyfingers", text: "Ladyfingers", id: 1000002 },
+  { class: "tiramisu-marscapone", text: "Marscapone Cheese", id: 1000003 },
 ];
 
-var cardFronts = document.querySelectorAll(".card-front");
+var cardFronts = document.querySelector(".card-front");
 var gameCards = document.querySelector("#gameCards");
 var startButton = document.querySelector(".startGame");
+var costText = document.querySelector('#cost');
+var revenueText = document.querySelector('#cost');
+var netProfitText = document.querySelector('#cost');
+var profitabilityText = document.querySelector('#cost');
 
 startButton.addEventListener("click", createCards);
 
 gameCards.addEventListener("click", handleClick);
 function handleClick(event) {
-  if (event.target.className.indexOf("card-back") === -1) {
+  if (event.target.className.indexOf("highlightCard") !== -1) {
     return;
   }
-  event.target.classList.add("hidden");
+  event.target.classList.add("highlightCard");
+ // highlightClick();
+  console.log(event);
 
   if (!firstCardClicked) {
     firstCardClicked = event.target;
-    firstCardClasses = firstCardClicked.previousElementSibling.className;
-  } else {
+    firstCardClasses = firstCardClicked.className;
+    firstCardID = firstCardClicked.id;
+
+  } else if(!secondCardClicked) {
     secondCardClicked = event.target;
-    secondCardClasses = secondCardClicked.previousElementSibling.className;
+    secondCardClasses = secondCardClicked.className;
+    secondCardID = secondCardClicked.id;
+  } else {
+    thirdCardClicked = event.target;
+    thirdCardClasses = thirdCardClicked.className;
+    thirdCardID = thirdCardClicked.id;
     gameCards.removeEventListener("click", handleClick);
     attempts++;
 
+//FIRST CARD CLICKED ID + SECOND CARD CLICKED ID + THIRD CARD CLICKED IF = ONE OF THE VALUES OF THAR DISH, THEN OUTPUT PICTURE OF DISH
+//IF
+
+    if (Number(firstCardID) + Number(secondCardID) + Number(thirdCardID) === 6) {
+      gameCards.addEventListener("click", handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+      thirdCardClicked = null;
+      revenue += 1200;
+      console.log("Bruschetta")
+    } else if (Number(firstCardID) + Number(secondCardID) + Number(thirdCardID) === 306) {
+      gameCards.addEventListener("click", handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+      thirdCardClicked = null;
+      revenue += 1200;
+      console.log("Fettuccine")
+    } else if (Number(firstCardID) + Number(secondCardID) + Number(thirdCardID) === 3006) {
+      gameCards.addEventListener("click", handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+      thirdCardClicked = null;
+      revenue += 1200;
+      console.log("Salad")
+    } else if (Number(firstCardID) + Number(secondCardID) + Number(thirdCardID) === 30006) {
+      gameCards.addEventListener("click", handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+      thirdCardClicked = null;
+      revenue += 1200;
+      console.log("Spaghetti")
+    } else if (Number(firstCardID) + Number(secondCardID) + Number(thirdCardID) === 300006) {
+      gameCards.addEventListener("click", handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+      thirdCardClicked = null;
+      revenue += 1200;
+      console.log("Tart")
+    } else if (Number(firstCardID) + Number(secondCardID) + Number(thirdCardID) === 3000006) {
+      gameCards.addEventListener("click", handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+      thirdCardClicked = null;
+      revenue += 1200;
+      console.log("Tiramisu")
+    } else {
+      console.log(firstCardID);
+      console.log(secondCardID);
+      console.log(thirdCardID);
+      console.log(firstCardID + secondCardID + thirdCardID);
+      highlightRemove();
+      return;
+    }
+
+/*
     if (firstCardClasses === secondCardClasses) {
       gameCards.addEventListener("click", handleClick);
       firstCardClicked = null;
@@ -67,7 +153,13 @@ function handleClick(event) {
         secondCardClicked = null;
       }, 1500);
     }
-    displayStats();
+    displayStats();*/
+  }
+}
+
+function highlightRemove() {
+  for (var i = 0; i < cardFronts.length; i++) {
+    cardFronts[i].className.remove("highlightCard");
   }
 }
 
@@ -91,6 +183,18 @@ function calculateAccuracy(attempts, matches) {
 var playAgain = document.querySelector("#play-again");
 playAgain.addEventListener("click", resetGame);
 
+//HIGHLIGHT THE CARD THAT IS CLICKED
+function highlightClick (event) {
+  if(event.className.split(' ').includes("main")){
+    console.log(event);
+    return
+  } else {
+    event.target.classList.add("highlightCard");
+    console.log(event);
+  }
+
+}
+
 function resetGame() {
   matches = 0;
   attempts = 0;
@@ -111,7 +215,7 @@ function resetCards() {
 
 function shuffleCards() {
   for (let i = cardFrontArray.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+    let j = Math.floor(Math.random() * (i + 1));
     [cardFrontArray[i], cardFrontArray[j]] = [
       cardFrontArray[j],
       cardFrontArray[i],
@@ -119,21 +223,22 @@ function shuffleCards() {
   }
 }
 
-function displayCards() {
-  for (var i = 0; i < cardFronts.length; i++) {
-    cardFronts[i].className = "card-front " + cardFrontArray[i];
-  }
-}
+
 
 function createCards() {
+  gameCards.classList.add("cardboard");
   document.querySelector(".startModal").classList.add("hidden");
   shuffleCards();
   for (var i = 0; i < maxMatches * 2; i++) {
     var cardContainer = document.createElement("div");
-    cardContainer.className = "card col-2";
-    var cardFront = createFrontCard(cardFrontArray[i]);
-    var cardBack = createBackCard();
-    cardContainer.append(cardFront, cardBack);
+    cardContainer.className = "card col-2 overlay";
+    var cardFront = createFrontCard(cardFrontArray[i]["class"]);
+    cardFront.setAttribute('id', cardFrontArray[i]["id"]);
+    var cardTextBox = createCardTextContainer(cardFrontArray[i]);
+    var cardBoxText = createCardText(cardFrontArray[i]["text"]);
+    cardTextBox.append(cardBoxText);
+    cardFront.append(cardTextBox);
+    cardContainer.append(cardFront);
     gameCards.append(cardContainer);
   }
 }
@@ -144,8 +249,15 @@ function createFrontCard(className) {
   return frontCard;
 }
 
-function createBackCard() {
-  var backCard = document.createElement("div");
-  backCard.classList.add("card-back");
-  return backCard;
+function createCardText(cardText) {
+  var cardTextEl = document.createElement("div");
+  cardTextEl.classList.add('card-text');
+  cardTextEl.textContent = cardText;
+  return cardTextEl;
+}
+
+function createCardTextContainer() {
+  var cardTextContain = document.createElement("div");
+  cardTextContain.classList.add("card-text-container");
+  return cardTextContain;
 }
